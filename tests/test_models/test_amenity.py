a2bfd42b4bedb26f23565ac7e7f23aee3667f311
inoublii <inoublii@gmail.com>
2020-02-21 18:33:09 +0100

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -21,17 +21,6 @@ class TestAmenity(unittest.TestCase):
         self.assertTrue(hasattr(self.b_inst, "name"))
         self.assertTrue(self.a_inst.created_at != self.b_inst.created_at)
 
-    def test_types(self):
-        """ Testing for types """
-        self.assertTrue(type(self.a_inst.created_at) is datetime.datetime)
-        self.assertTrue(type(self.a_inst.name) is str)
-
-    def test_save(self):
-        """ Testing updating  """
-        b_date = self.b_inst.updated_at
-        self.b_inst.save()
-        b_date2 = self.b_inst.updated_at
-        self.assertTrue(b_date != b_date2)
 
 if __name__ == '__main__':
     unittest.main()
