0d30dbe7e883b35256d740920a8845af7052632a
inoublii <inoublii@gmail.com>
2020-02-21 18:39:37 +0100

    tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -34,10 +34,7 @@ class TestState(unittest.TestCase):
         b_date = self.b_inst.updated_at
         self.b_inst.save()
         b_date2 = self.b_inst.updated_at
-
-
-
-		self.assertTrue(b_date != b_date2)
+        self.assertTrue(b_date != b_date2)
 
 if __name__ == '__main__':
     unittest.main()
