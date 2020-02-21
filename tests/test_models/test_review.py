a4a7b2b02e943e779955723a34428e5d6f6e697d
inoublii <inoublii@gmail.com>
2020-02-21 18:36:42 +0100

    tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -37,10 +37,6 @@ class TestReview(unittest.TestCase):
         b_date = self.b_inst.updated_at
         self.b_inst.save()
         b_date2 = self.b_inst.updated_at
-
-
-
-		
         self.assertTrue(b_date != b_date2)
 
 if __name__ == '__main__':
